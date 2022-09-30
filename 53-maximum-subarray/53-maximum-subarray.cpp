6c4c6313a940b9d72a97ddb9ec370549ce86da74
raviteja-reddy-guntaka <114640238+raviteja-reddy-guntaka@users.noreply.github.com>
2022-09-29 23:05:05 -0700

    Time: 175 ms (58.07%), Space: 67.7 MB (53.18%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -5,7 +5,8 @@ public:
         int max_s = INT_MIN, curr = 0;
         for (int i=0; i<nums.size(); i++) {
             curr += nums[i];
-            max_s = max(max_s, max(curr, nums[i]));
+            max_s = max(max_s, curr);
+            max_s = max(max_s, nums[i]);
             if (curr < 0)
                 curr = 0;
         }
