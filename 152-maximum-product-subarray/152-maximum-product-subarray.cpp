a5b75aea513159819a54b5b7959aaf229fc36c1b
Raviteja Reddy Guntaka <114640238+raviteja-reddy-guntaka@users.noreply.github.com>
2022-10-08 23:19:54 -0700

    Time: 23 ms (10.12%), Space: 14.1 MB (7.49%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -7,16 +7,10 @@ public:
         dp[0][1] = 1;
         
         for (int i=1; i<=nums.size(); i++) {
-            if (nums[i-1] != 0) {
-                int up = dp[i-1][0]*nums[i-1], down = dp[i-1][1]*nums[i-1];
-                dp[i][0] = max(up, max(nums[i-1], down));
-                dp[i][1] = min(up, min(nums[i-1], down));
-                max_p = max(max_p, dp[i][0]);
-            } else {
-                max_p = max(max_p, 0);
-                dp[i][0] = 1;
-                dp[i][1] = 1;
-            }
+            int up = dp[i-1][0]*nums[i-1], down = dp[i-1][1]*nums[i-1];
+            dp[i][0] = max(up, max(nums[i-1], down));
+            dp[i][1] = min(up, min(nums[i-1], down));
+            max_p = max(max_p, dp[i][0]);
         }
         return max_p;
     }
