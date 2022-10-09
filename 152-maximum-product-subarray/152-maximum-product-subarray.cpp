d8f3885e682993f58a8ce23a36a3acd215afb060
Raviteja Reddy Guntaka <114640238+raviteja-reddy-guntaka@users.noreply.github.com>
2022-10-08 23:23:36 -0700

    Time: 10 ms (73.64%), Space: 14 MB (7.49%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         
-        int dp[nums.size()+1][2], max_p = INT_MIN;
-        dp[0][0] = 1;
-        dp[0][1] = 1;
+        int dp[nums.size()][2], max_p = nums[0];
+        dp[0][0] = nums[0];
+        dp[0][1] = nums[0];
         
-        for (int i=1; i<=nums.size(); i++) {
-            int up = dp[i-1][0]*nums[i-1], down = dp[i-1][1]*nums[i-1];
-            dp[i][0] = max(up, max(nums[i-1], down));
-            dp[i][1] = min(up, min(nums[i-1], down));
+        for (int i=1; i<nums.size(); i++) {
+            int up = dp[i-1][0]*nums[i], down = dp[i-1][1]*nums[i];
+            dp[i][0] = max(up, max(nums[i], down));
+            dp[i][1] = min(up, min(nums[i], down));
             max_p = max(max_p, dp[i][0]);
         }
         return max_p;
