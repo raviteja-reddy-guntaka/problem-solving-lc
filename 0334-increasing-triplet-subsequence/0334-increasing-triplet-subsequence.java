2e41f26bb4b07f4c5c599dafe6539d8c763c1681
Raviteja Reddy Guntaka <114640238+raviteja-reddy-guntaka@users.noreply.github.com>
2022-10-13 01:42:04 -0700

    Time: 13 ms (11.55%), Space: 93.5 MB (48.09%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,19 +1,29 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
         
-        int n = nums.length;
-        int[] dp = new int[n];
-        int len = 0;
+//         int n = nums.length;
+//         int[] dp = new int[n];
+//         int len = 0;
         
-        for (int x:nums) {
-            int idx = Arrays.binarySearch(dp, 0, len, x);
-            if (idx < 0)
-                idx = -(idx+1);
-            dp[idx] = x;
-            if (idx == len)
-                len++;
+//         for (int x:nums) {
+//             int idx = Arrays.binarySearch(dp, 0, len, x);
+//             if (idx < 0)
+//                 idx = -(idx+1);
+//             dp[idx] = x;
+//             if (idx == len)
+//                 len++;
             
-            if (len==3)
+//             if (len==3)
+//                 return true;
+//         }
+        
+        int min = Integer.MAX_VALUE, s_min = Integer.MAX_VALUE;
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i]<=min)
+                min = nums[i];
+            else if (nums[i]<s_min)
+                s_min = nums[i];
+            else if (nums[i]>s_min)
                 return true;
         }
         
