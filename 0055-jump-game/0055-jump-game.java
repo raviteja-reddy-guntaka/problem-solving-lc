cf3a98c7ca0b8a4dad90e5dfbca4702ea0661fb1
Raviteja Reddy Guntaka <114640238+raviteja-reddy-guntaka@users.noreply.github.com>
2022-11-27 16:46:53 -0700

    Time: 2 ms (94.52%), Space: 43.1 MB (84.32%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,19 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
+        int farthest = 0;
         
-        boolean[] result = new boolean[nums.length];
-        result[0] = true;
-        
-        for (int i=0; i<nums.length-1 && !result[nums.length-1]; i++) {
-            if (result[i]) {
-                for (int j=1; j<=nums[i] && i+j<nums.length; j++) {
-                    result[i+j] = true;
-                }
-            } else {
+        for (int i=0; i<nums.length; i++) {
+            if (farthest < i)
                 return false;
-            }
+            farthest = Math.max(farthest, i+nums[i]);
         }
         
-        return result[nums.length-1];
+        return true;
     }
 }
