b621df345d26d3e0af8add8f3cd722c3476bad5f
Raviteja Reddy Guntaka <114640238+raviteja-reddy-guntaka@users.noreply.github.com>
2022-12-04 00:32:55 -0700

    Time: 10 ms (42.47%), Space: 41.8 MB (73.61%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,25 +1,26 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
         int n = s.length();
-        Map<Character, Integer> first = new HashMap<>();
-        Map<Character, Integer> second = new HashMap<>();
+        Set<Character> sett = new HashSet<>();
+        sett.add('a');
+        sett.add('e');
+        sett.add('i');
+        sett.add('o');
+        sett.add('u');
+        sett.add('A');
+        sett.add('E');
+        sett.add('I');
+        sett.add('O');
+        sett.add('U');
+        int firstCount = 0;
+        int secondCount = 0;
         for (int i=0; i<n/2; i++) {
-            if (first.get(s.charAt(i)) == null)
-                first.put(s.charAt(i), 0);
-            first.put(s.charAt(i), 1 + first.get(s.charAt(i)));
-            if (second.get(s.charAt(n/2 + i)) == null)
-                second.put(s.charAt(n/2 + i), 0);
-            second.put(s.charAt(n/2 + i), 1 + second.get(s.charAt(n/2 + i)));
+            if (sett.contains(s.charAt(i)))
+                firstCount++;
+            if (sett.contains(s.charAt(n/2 + i)))
+                secondCount++;
+            
         }
-        int firstCount = first.getOrDefault('a', 0) + first.getOrDefault('e', 0) + 
-            first.getOrDefault('i', 0) + first.getOrDefault('o', 0) + first.getOrDefault('u', 0) +
-            first.getOrDefault('A', 0) + first.getOrDefault('E', 0) + first.getOrDefault('I', 0) +
-            first.getOrDefault('O', 0) + first.getOrDefault('U', 0);
-        
-        int secondCount = second.getOrDefault('a', 0) + second.getOrDefault('e', 0) + 
-            second.getOrDefault('i', 0) + second.getOrDefault('o', 0) + second.getOrDefault('u', 0) +
-            second.getOrDefault('A', 0) + second.getOrDefault('E', 0) + second.getOrDefault('I', 0) +
-            second.getOrDefault('O', 0) + second.getOrDefault('U', 0);
         
         if (firstCount != secondCount)
             return false;
